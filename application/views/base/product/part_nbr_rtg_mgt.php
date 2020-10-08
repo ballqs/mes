59f4f417f069ffe0540f8f677a917f76bf0b503c
hojin <ballqs_@naver.com>
2020-10-08 11:05:21 +0900

    원하는것만 바꿈

--- application/views/base/product/part_nbr_rtg_mgt.php
+++ application/views/base/product/part_nbr_rtg_mgt.php
@@ -5,15 +5,16 @@
     <div class="searchWrap cf">
         <div class="searchLine">
             <div class="boxW col-3 select">
-                <p>공장</p>
+                <p>공11111장</p>
+                <p>장111111공</p>
                 <select name="fact_cd"></select>
             </div>
             <div class="boxW col-3 select" style="display: none;">
-                <p>사업자</p>
+                <p>사222업3333자</p>
                 <select name="cmpny_cd"></select>
             </div>
             <div class="boxW col-3 search">
-                <p>품번</p>
+                <p>품444444번</p>
                 <div class="popW">
                     <input type="text" name="prt_nbr_cd" value="">
                     <a class="popBt" name="btn_pop_prt_nbr_cd"><i class="far fa-window-restore"></i></a>
